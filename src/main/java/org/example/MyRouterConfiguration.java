53b88fde3ce01636b9e3642de78d161eab884bba
iaptekar <iaptekar@gmail.com>
2020-11-09 20:53:18 +0000

    use advice with

--- src/main/java/org/example/MyRouterConfiguration.java
+++ src/main/java/org/example/MyRouterConfiguration.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.component.jms.JmsConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,10 +17,19 @@ public class MyRouterConfiguration {
 
             @Override
             public void configure() throws Exception {
-                from("{{route.start}}").log("message ${body}").to("{{route.end}}");
+                from("{{route.start}}").routeId("test").log("message ${body}").to("{{route.end}}");
             }
 
         };
     }
 
+    private static JmsComponent createArtemisComponent() {
+        // Sets up the Artemis core protocol connection factory
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+
+        JmsConfiguration configuration = new JmsConfiguration();
+        configuration.setConnectionFactory(connectionFactory);
+
+        return new JmsComponent(configuration);
+    }
 }
--- src/test/java/org/example/MyApplicationTest.java
+++ src/test/java/org/example/MyApplicationTest.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
-import org.apache.camel.test.spring.junit5.MockEndpoints;
+import org.apache.camel.test.spring.junit5.UseAdviceWith;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -15,15 +18,29 @@ import org.springframework.test.context.TestPropertySource;
 @CamelSpringBootTest
 @SpringBootTest
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
-@TestPropertySource(properties = {"spring.flyway.enabled=false", "route.end=mock:end"})
-@MockEndpoints("direct:end")
+@TestPropertySource(properties = {"spring.flyway.enabled=false"})
+//@MockEndpoints("direct:end")
+@UseAdviceWith
 public class MyApplicationTest {
 
     @Autowired
     private ProducerTemplate template;
 
-    @EndpointInject("mock:end")
+    @EndpointInject("mock:direct:end")
     private MockEndpoint mock;
+
+    @Autowired
+    private CamelContext context;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        AdviceWithRouteBuilder.adviceWith(context, "test", route -> {
+            route.interceptSendToEndpoint("{{route.end}}")
+                    .skipSendToOriginalEndpoint()
+                    .to("mock:direct:end");
+        });
+        context.start();
+    }
 
     @Test
     public void testReceive() throws Exception {
